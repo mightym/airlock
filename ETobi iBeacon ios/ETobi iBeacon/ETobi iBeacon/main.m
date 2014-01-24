4122f75d7dea4029fb9761ae2cd5bd8d19e8075c
Tobias Liebig <work@etobi.de>
2014-01-24 23:06:11 +0100

    add test app to experiment with ibeacons on ios

---
+++ ETobi iBeacon ios/ETobi iBeacon/ETobi iBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ETobi iBeacon
+//
+//  Created by Tobias Liebig on 24.01.14.
+//  Copyright (c) 2014 Tobias Liebig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ETOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ETOAppDelegate class]));
+    }
+}
